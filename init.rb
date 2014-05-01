14d6fe7cc75e451e57b792145536497215b74ffb
Jochen Roth <rothjochen@gmail.com>
2014-05-02 01:23:00 +0200

    adding translations

--- init.rb
+++ init.rb
@@ -5,6 +5,8 @@ Redmine::Plugin.register :gitdownload do
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
+  
+    settings :default => {'empty' => true}, :partial => 'settings/gitdownload'
 end
 
 # VIEW HOOKS
