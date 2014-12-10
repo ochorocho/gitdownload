b41fb16b3f86da38b8673ac103052008b179b6f8
ochorocho <rothjochen@gmail.com>
2014-12-10 11:10:43 +0100

    Update script.js

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -61,7 +61,7 @@ $(function() {
 	});
 	$('.gitRadio input').click(function() {
 		$('.gitRadio input').not($(this)).removeAttr('checked');
-		if ($(this).attr('checked') !== 'checked') {
+		if ($(this).attr('checked') == 'checked') {
 			$(this).removeAttr('checked');
 			$('#flash_notice').remove();
 		} else {
