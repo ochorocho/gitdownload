a84df3022acd40bde400c698adbc8826bc6e746e
ochorocho <root@knallimall.org>
2017-04-15 22:44:24 +0200

    Optimize js, add editorconfig file

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -1,90 +1,87 @@
 $(function() {
-	$("#gitOptions").dialog({
-		autoOpen: false,
-		modal: true,
-		width: 500,
-		height: 340,
-		buttons: {
-			Download: function() {
-				var url = GITDOWNLOAD_CONTROLLER;
-				var branch = $('#gitBranch option:selected').val();
-				var changeset = $('#revs input:checked').val();
-				var params = '';
-				if (branch !== '') {
-					params += 'archive=' + branch;
-				} else if (changeset !== undefined) {
-					params += 'archive=' + changeset;
-				}
-				if (params === '') {
-					$('#revs').before('<div id="flash_notice" class="flash error">' + GITDOWNLOAD_ERROR + '</div>');
-				} else {
-					$('#flash_notice').remove();
-					params += '&repository=' + GITDOWNLOAD_ID;
-					params += '&identifier=' + GITDOWNLOAD_REPO;
-					params += '&type=' + $('#gitType').val();
-					params += '&gitFormat=' + $('#gitFormat option:selected').val();
-
-					var spinner = '<div id="generateSpinner">';
-					spinner += '<div class="spinner"><div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div><div class="rect5"></div></div>';
-					spinner += '<div class="spinText">Archiving in progress ...</div></div>';
-					$('body').prepend(spinner);
-					
-					$(document).keyup(function(e) {
-						if (e.keyCode == 27) {
-							$('#generateSpinner').remove();
-						}
-					});
-										
-					$.ajax({
-						dataType: "json",
-						url: url,
-						data: params,
-						type: 'POST',
-						success: function(data) {
-							$('#generateSpinner').remove();
-							$('#frame').html('<iframe src="' + url + '?filename=' + data.filename + '"></iframe>');
-						},
-						error: function(data) {
-							alert('Error: Please reload!');
-						}
-					});
-				}
-			},
-			Close: function() {
-				$(this).dialog("close");
-			}
-		}
-	});
-	$('#gitOptions object').blur();
-	$('#gitIcon').click(function() {
-		$("#gitOptions").dialog("open");
-	});
-	$('.gitRadio input').click(function() {
-		$('.gitRadio input').not($(this)).removeAttr('checked');
-		if ($(this).attr('checked') == 'checked') {
-			$(this).removeAttr('checked');
-			$('#flash_notice').remove();
-		} else {
-			$(this).attr('checked', 'checked');
-			$('#flash_notice').remove();
-		}
-	});
-	$('select#gitBranch').on('change', function(e) {
-		$('#flash_notice').remove();
-		var valueSelected = this.value;
-		if (valueSelected !== '') {
-			$('#revs').addClass('disabled');
-			$('#revs input').removeAttr('checked').attr('disabled', 'disabled');
-		} else {
-			$('#revs').removeClass('disabled');
-			$('#revs input').removeAttr('disabled');
-		}
-	});
-	if ($('#gitBranch option:selected').val() !== '') {
-		$('#revs').addClass('disabled');
-		$('#revs input').removeAttr('checked').attr('disabled', 'disabled');
-	} else {
-		$('#revs').removeClass('disabled');
-		$('#revs input').removeAttr('disabled');
-	}
+    $("#gitOptions").dialog({
+        autoOpen: false,
+        modal: true,
+        width: 500,
+        height: 340,
+        buttons: {
+            Download: function() {
+                var url = GITDOWNLOAD_CONTROLLER;
+                var branch = $('#gitBranch option:selected').val();
+                var changeset = $('#revs input:checked').val();
+                var params = '';
+                if (branch !== '') {
+                    params += 'archive=' + branch;
+                } else if (changeset !== undefined) {
+                    params += 'archive=' + changeset;
+                }
+                if (params === '') {
+                    $('#revs').before('<div id="flash_notice" class="flash error">' + GITDOWNLOAD_ERROR + '</div>');
+                } else {
+                    $('#flash_notice').remove();
+                    params += '&repository=' + GITDOWNLOAD_ID;
+                    params += '&identifier=' + GITDOWNLOAD_REPO;
+                    params += '&type=' + $('#gitType').val();
+                    params += '&gitFormat=' + $('#gitFormat option:selected').val();
+                    var spinner = '<div id="generateSpinner">';
+                    spinner += '<div class="spinner"><div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div><div class="rect5"></div></div>';
+                    spinner += '<div class="spinText">Archiving in progress ...</div></div>';
+                    $('body').prepend(spinner);
+                    $(document).keyup(function(e) {
+                        if (e.keyCode == 27) {
+                            $('#generateSpinner').remove();
+                        }
+                    });
+                    $.ajax({
+                        dataType: "json",
+                        url: url,
+                        data: params,
+                        type: 'POST',
+                        success: function(data) {
+                            $('#generateSpinner').remove();
+                            $('#frame').html('<iframe src="' + url + '?filename=' + data.filename + '"></iframe>');
+                        },
+                        error: function() {
+                            alert('Error: Please reload!');
+                        }
+                    });
+                }
+            },
+            Close: function() {
+                $(this).dialog("close");
+            }
+        }
+    });
+    $('#gitOptions object').blur();
+    $('#gitIcon').click(function() {
+        $("#gitOptions").dialog("open");
+    });
+    $('.gitRadio input').click(function() {
+        $('.gitRadio input').not($(this)).removeAttr('checked');
+        if ($(this).attr('checked') == 'checked') {
+            $(this).removeAttr('checked');
+            $('#flash_notice').remove();
+        } else {
+            $(this).attr('checked', 'checked');
+            $('#flash_notice').remove();
+        }
+    });
+    $('select#gitBranch').on('change', function() {
+        $('#flash_notice').remove();
+        var valueSelected = this.value;
+        if (valueSelected !== '') {
+            $('#revs').addClass('disabled');
+            $('#revs input').removeAttr('checked').attr('disabled', 'disabled');
+        } else {
+            $('#revs').removeClass('disabled');
+            $('#revs input').removeAttr('disabled');
+        }
+    });
+    if ($('#gitBranch option:selected').val() !== '') {
+        $('#revs').addClass('disabled');
+        $('#revs input').removeAttr('checked').attr('disabled', 'disabled');
+    } else {
+        $('#revs').removeClass('disabled');
+        $('#revs input').removeAttr('disabled');
+    }
 });
